11ee3b5588f50e5b9ac3c0e318522cb86163ddaf
YashRawat21 <rawat17yash@gmail.com>
2023-07-12 20:39:08 +0530

    update changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
  import "./styles.css";
-const Header = () => {
-  return(
-    <div className="header">
-      <div className = "logo-container">
-      <img src = "Food Fire Logo.png"  className="logoImg"/>
-      </div>
-    
-      <div className="navItems">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li> Cart</li>
-        </ul>
-      </div>
-    </div>
-  )
-}
+import Header from "./components/Header";
+
+
 const restaurantList = [
   {
     type: "restaurant",
