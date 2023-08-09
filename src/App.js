c857a344625895b44b1d634f7b2af2195165ed7d
YashRawat21 <rawat17yash@gmail.com>
2023-08-09 19:41:14 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import UserContext from "./components/utils/UserContext";
+import appStore from "./components/utils/appStore";
+import {Provider} from "react-redux"
+
 
 function App() {
    const [userName , setUserName] = useState();
@@ -15,12 +18,17 @@ function App() {
     setUserName(data.name)
    },[])
   return (
+  
+      <Provider store = {appStore}>
     <UserContext.Provider value={{loggedIn : userName ,setUserName}}>
     <div className="App">
       <Header />
         <Outlet />
     </div>
     </UserContext.Provider>
+
+      </Provider>
+
   );
 }
 
