95a8f07c1d051e29e96861c09d0d0dd55eb460c9
YashRawat21 <rawat17yash@gmail.com>
2023-08-09 21:36:02 +0530

    update commit

--- src/components/utils/cartSlice.js
+++ src/components/utils/cartSlice.js
@@ -1,19 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-    name:"cart",
-    initialState: {
-        items:[]
+    name: 'cart',
+    initialState : {
+        items : []
     },
     reducers : {
         addItem : (state,action) => {
-            state.items.push(action.payload)
+           state.items.push(action.payload)
         },
         removeItem : (state,action) => {
-            state.items.pop(action.payload)
+            state.items.pop()
         },
         clearCart : (state) => {
             state.items.length = 0;
         }
     }
 })
+export const {addItem , removeItem , clearCart} = cartSlice.actions
+export default cartSlice.reducer;
